dcfe3239c0c063e494b2cffb9c87ef499541e8a4
Kris <krisroqueee@gmail.com>
2025-10-28 20:15:17 +0800

    putting some darkmode (not interactable yet)

--- publicdeploy/script.js
+++ publicdeploy/script.js
@@ -1,11 +1,9 @@
 // Uses DOM, attach darkMode to window so inline onclick works
 document.addEventListener('DOMContentLoaded', function () {
     var element = document.body;
-    var everything = document.getElementsByClassName('everything');
 
     window.darkMode = function () {
         element.classList.toggle('darkMode');
-        everything.classList.toggle('darkMode');
     };
 });
 
