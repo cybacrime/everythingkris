ec9cf05aa7cc2a13243b5db26e26605cc4a63e0c
Kris <krisroqueee@gmail.com>
2025-10-30 12:30:21 +0800

    replaced top ad with bannerlink, removed scrollbar in main iframe, made link buttons a little bigger. but link buttons are subject to change (design)

--- publicdeploy/script.js
+++ publicdeploy/script.js
@@ -1,9 +1,1 @@
-// Uses DOM, attach darkMode to window so inline onclick works
-document.addEventListener('DOMContentLoaded', function () {
-    var element = document.body;
-
-    window.darkMode = function () {
-        element.classList.toggle('darkMode');
-    };
-});
-
+// n.a //
